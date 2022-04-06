cc289781797eb6a16ed517773519a519caa25689
Eduard Blanco <eblancode@gmail.com>
2022-04-06 19:59:14 +0200

    Upload

---
+++ lib/addresses.ts
@@ -0,0 +1,9 @@
+import { PublicKey } from "@solana/web3.js"
+
+// This is your shop address
+//export const shopAddress = new PublicKey('...')
+export const shopAddress = new PublicKey('EXWr1Go8UyfA39U1dfRkcH8uUvqunbRhQEUQNm36UsyQ')
+// This is the same for everyone
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// This is your token/coupon address
+export const couponAddress = new PublicKey('74aFyMgbKppdab4wEVz5AXL3UGbxMkwkwgK2LVzn3MmG')
